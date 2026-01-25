30f5437e93be20d92964367606bbbd68d9029ddb
vishakhviswan <vishakhviswan1995@gmail.com>
2026-01-25 15:58:50 +0530

    error cleared

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const supabase = require("../supabaseClient");
+const supabase = require("../config/supabase");
 
 /* ================= CREATE ORDER ================= */
 router.post("/", async (req, res) => {
